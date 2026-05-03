4da43db1d48580286fd4af680faa56df6fca9109
sourabhgouda <sourabhgouda19@gmail.com>
2026-05-03 14:49:28 +0530

    Added the notes for Introductions and variables_and_Datatype  folder

---
+++ 2 Variables and Data_Types/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String args[]){
+        int num = 10; // Integer Type
+        double height = 5.2; // Float / Double Type
+        boolean isStudent = true; // Boolean Type 
+        String name = "Sourabhgouda"; // String Type
+        char grade = 'A'; // Character Type
+        System.out.println("name: "+name);
+        System.out.println("height: "+height);
+        System.out.println("isStudent: "+isStudent);
+        System.out.println("num: "+num);
+        System.out.println("grade: "+grade); 
+    }
+}
