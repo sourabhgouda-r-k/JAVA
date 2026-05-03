b697ae8a69930578e92d3619015459e7898174b5
sourabhgouda <sourabhgouda19@gmail.com>
2026-05-03 14:50:58 +0530

    Added the notes for Introductions and variables_and_Datatype  folder

--- Variables.java
+++
@@ -1,8 +0,0 @@
-public class Variables {
-    public static void main(String args[]){
-        int num1 = 10;
-        int num2 = 20;
-        int  result = num1+num2;
-        System.out.println(result);
-    }
-}
