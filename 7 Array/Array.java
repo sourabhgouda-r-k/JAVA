92d3cc666aeeedab6e3902d394752455cd84e81d
sourabhgouda <sourabhgouda19@gmail.com>
2026-05-04 18:58:59 +0530

    Added the Arrays and loops

---
+++ 7 Array/Array.java
@@ -0,0 +1,25 @@
+public class Array {
+    public static void main(String[] args) {
+        
+        // 2 ways to declare arrays 
+        // old method
+        int arr[]={1,2,3,4};
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+
+
+        // new method
+        int arr1[];
+        arr1 = new int[]{10,20,30,40};
+          System.out.println(arr1[0]);
+        System.out.println(arr1[1]);
+        System.out.println(arr1[2]);
+        System.out.println(arr1[3]);
+
+
+        
+    }
+    
+}
